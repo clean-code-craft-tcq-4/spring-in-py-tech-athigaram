3af3f6d4b50d954ca4d7589647fdef124a60ec76
tech-athigaram <65674906+tech-athigaram@users.noreply.github.com>
2022-05-09 19:58:55 +0530

    Comments added and alert object type check also added

--- alerts.py
+++ alerts.py
@@ -1,8 +1,10 @@
+#interface for all Alert type of objects
 class Alert:
     def alert(self):
         print("""Sending Alert""")
         pass
-    
+
+# Class for Mail Alert 
 class EmailAlert(Alert):
     def __init__(self):
         self.emailSent = False
@@ -10,13 +12,15 @@ class EmailAlert(Alert):
         print("""Sending mail alert""")
         self.emailSent = True
 
+# Class for Mail Alert 
 class LEDAlert(Alert):
     def __init__(self):
         self.ledGlows = False
     def alert(self):
         print("""Sending LED alert""")
         self.ledGlows = True
-          
+
+# Class for Status Alert           
 class StatsAlerter:
     def __init__(self,maxThreshold,alerts=[]):
         self.maxThreshold = maxThreshold
@@ -24,5 +28,10 @@ class StatsAlerter:
     
     def checkAndAlert(self,numbers):
         if (max(numbers)>self.maxThreshold):
+            # iterating all type of alerts and triggering alerts.
             for alert in self.alerts:
+                # checking the object type before triggering alert
+                if(isinstance(alert,Alert)):
                     alert.alert();
+                else :
+                    print(str(type(alert))+ " is not a alert type object.")
