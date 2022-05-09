c95cfa239bf6626c6f5ae9e7bda1ead8baa52ff1
tech-athigaram <65674906+tech-athigaram@users.noreply.github.com>
2022-05-09 17:09:46 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,44 @@
+import math as maths
+import statistics as stats
 
 def calculateStats(numbers):
-  return None
+    if not numbers or any(maths.isnan(number) for number in numbers):
+        return statsAssigner(maths.nan,maths.nan,maths.nan)
+    else:
+        return statsAssigner(stats.mean(numbers),min(numbers),max(numbers))
+
+def statsAssigner(stats_avg,stats_min,stats_max):
+    computedStats = {}
+    computedStats['avg']= stats_avg
+    computedStats['min']= stats_min
+    computedStats['max']= stats_max
+    return computedStats
+
+class Alert:
+    def alert(self):
+        print("""Sending Alert""")
+        pass
+    
+class EmailAlert(Alert):
+    def __init__(self):
+        self.emailSent = False
+    def alert(self):
+        print("""Sending mail alert""")
+        self.emailSent = True
+
+class LEDAlert(Alert):
+    def __init__(self):
+        self.ledGlows = False
+    def alert(self):
+        print("""Sending LED alert""")
+        self.ledGlows = True
+          
+class StatsAlerter:
+    def __init__(self,maxThreshold,alerts=[]):
+        self.maxThreshold = maxThreshold
+        self.alerts = alerts
+    
+    def checkAndAlert(self,numbers):
+        if (max(numbers)>self.maxThreshold):
+            for alert in self.alerts:
+                    alert.alert();
