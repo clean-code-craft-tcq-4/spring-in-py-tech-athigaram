979da7534efca89c0d4dba7338f3c5278a55805f
tech-athigaram <65674906+tech-athigaram@users.noreply.github.com>
2022-05-09 17:26:07 +0530

    moved alerts to seperate module

--- statistics.py
+++ statistics.py
@@ -15,32 +15,3 @@ def statsAssigner(stats_avg,stats_min,stats_max):
     computedStats['min']= stats_min
     computedStats['max']= stats_max
     return computedStats
-
-class Alert:
-    def alert(self):
-        print("""Sending Alert""")
-        pass
-    
-class EmailAlert(Alert):
-    def __init__(self):
-        self.emailSent = False
-    def alert(self):
-        print("""Sending mail alert""")
-        self.emailSent = True
-
-class LEDAlert(Alert):
-    def __init__(self):
-        self.ledGlows = False
-    def alert(self):
-        print("""Sending LED alert""")
-        self.ledGlows = True
-          
-class StatsAlerter:
-    def __init__(self,maxThreshold,alerts=[]):
-        self.maxThreshold = maxThreshold
-        self.alerts = alerts
-    
-    def checkAndAlert(self,numbers):
-        if (max(numbers)>self.maxThreshold):
-            for alert in self.alerts:
-                    alert.alert();
